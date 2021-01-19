da2b5b2f625de14af9b28d13f0bcb6456515235c
LeeHyeonKyu <hklee128@naver.com>
2021-01-19 18:18:10 +0900

    2nd upload

--- baseball_game.py
+++ baseball_game.py
@@ -93,7 +93,7 @@ def is_duplicated_number(three_digit):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
 
-    result = len(set(three_digit)) < 3
+    result = len(set(str(three_digit))) < 3
     # ==================================
     return result
     
